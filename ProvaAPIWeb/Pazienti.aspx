c05fe751bea2e905fb8d41dcb5df5afb6ebed777
LorenzoChianella <lore.chiane@hotmail.it>
2017-05-19 16:01:49 +0200

    Visualizzazione Pazienti

---
+++ ProvaAPIWeb/Pazienti.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pazienti.aspx.cs" Inherits="ProvaAPIWeb.Pazienti" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="581px">
+            <Columns>
+                <asp:CommandField ShowEditButton="True" />
+                <asp:CommandField ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ProvaAPIWeb/Pazienti.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProvaAPIWeb
+{
+    public partial class Pazienti : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                RootObject<Paziente> paz;
+                paz = ApiRestClient.GetPazientiDataFromServer();
+
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[15] {
+                            new DataColumn("Nome", typeof(string)),
+                            new DataColumn("Cognome", typeof(string)),
+                            new DataColumn("Sesso", typeof(string)),
+                            new DataColumn("Data Nascita", typeof(string)),
+                            new DataColumn("Luogo", typeof(string)),
+                            new DataColumn("Codice Fiscale", typeof(string)),
+                            new DataColumn("Residenza", typeof(string)),
+                            new DataColumn("Provincia", typeof(string)),
+                            new DataColumn("Indirizzo", typeof(string)),
+                            new DataColumn("CAP", typeof(string)),
+                            new DataColumn("Telefono", typeof(string)),
+                            new DataColumn("Mobile", typeof(string)),
+                            new DataColumn("Email", typeof(string)),
+                            new DataColumn("Codice Sanitario", typeof(string)),
+                            new DataColumn("Codice Medico", typeof(string))
+                });
+
+                for (int i = 0; i < paz.items; i++)
+                {
+                    dt.Rows.Add(paz.data[i].nome, paz.data[i].cognome, paz.data[i].Sesso,  paz.data[i].data_nascita,paz.data[i].luogo,
+                        paz.data[i].cod_fis, paz.data[i].residenza, paz.data[i].provincia, paz.data[i].indirizzo,
+                        paz.data[i].cap, paz.data[i].telefono, paz.data[i].mobile, paz.data[i].email, paz.data[i].cod_sanitario, paz.data[i].cod_medico);
+                }
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+                //GridView1.Columns[1].Visible = false;
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
